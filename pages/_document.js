0ea98f5e24ebbe07e827b1f6a0abbe07549243d2
Jason King <jking@cab408.com>
2020-02-06 19:57:34 -0500

    Titles

--- pages/_document.js
+++ pages/_document.js
@@ -33,12 +33,12 @@ export default class MyDocument extends Document {
       <html lang="en">
         <Head>
           <meta charSet="UTF-8" />
-          <meta name="description" content="National Park Service: A State-by-State Guide" />
+          <meta name="description" content="Jason King: Creative Technologist Developer/Designer/Artist" />
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <meta name="theme-color" content="#00ac47" />
           <link rel="icon" href="/favicon.ico" />
           <link rel="manifest" href="/manifest.json" />
-          <meta name="apple-mobile-web-app-title" content="National Parks" />
+          <meta name="apple-mobile-web-app-title" content="JSNKNG" />
           <meta name="apple-mobile-web-app-status-bar-style" content="default" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="mobile-web-app-capable" content="yes" />
